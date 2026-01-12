b9637acbbde2d12140e126ac999e27b4889fb850
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-12 16:03:43 +0100

    Add Demo05 OfType<T> VS Cast<T>

---
+++ Demo05/EtreVivant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05
+{
+    public class EtreVivant
+    {
+        const int MAX_POINT_DE_VIE = 10;
+        public int PointDeVie { get; set; }
+    }
+}
---
+++ Demo05/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Demo05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<EtreVivant> aquariums = new List<EtreVivant>()
+            {
+                new Algue(),
+                new Algue(),
+                new Algue(),
+                new Poisson() { Name = "Némo" },
+                new Poisson() { Name = "Dorie" },
+                new Poisson() { Name = "Marlin" }
+            };
+
+            IEnumerable<Poisson> poissons = aquariums.OfType<Poisson>();
+
+            foreach (var p in poissons)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            try
+            {
+                IEnumerable<Algue> algues = aquariums.Cast<Algue>();
+
+                Console.WriteLine($"Il y a {algues.Count()} algues dans l'aquarium.");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Au moins un élément n'est pas (compatible) Algue");
+            }
+        }
+    }
+}
