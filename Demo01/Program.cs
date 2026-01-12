4677434908c288faacb6a978eedcabfaa84c88f7
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-12 10:10:43 +0100

    Ajoutez des fichiers projet.

---
+++ Demo01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Demo01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud = new Student()
+            {
+                Matricule = 1,
+                FirstName = "Anaïs",
+                LastName = "Kadri",
+                SubscribeDate = new DateOnly(2025, 3, 15)
+            };
+
+            var person = new { 
+                stud.Matricule,
+                Prenom = stud.FirstName , 
+                NomDeFamille = stud.LastName,
+                Role = (stud is Student)?"Étudiant" : "Professeur"
+            };
+
+            Console.WriteLine($"Bonjour, je vous présente {person.Prenom} {person.NomDeFamille}, elle est {person.Role}.");
+        }
+    }
+}
